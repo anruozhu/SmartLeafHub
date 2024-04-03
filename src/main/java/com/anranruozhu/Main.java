2e36109fb207fee34f68c087696ba2c2e2d22238
anranruozhu <1485156774@qq.com>
2024-04-03 09:53:53 +0800

    进行基本项目构建

--- src/main/java/com/anranruozhu/Main.java
+++ src/main/java/com/anranruozhu/Main.java
@@ -1,7 +1,13 @@
 package com.anranruozhu;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+@SpringBootApplication
 public class Main {
+    //项目启动
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class,args);
     }
 }
