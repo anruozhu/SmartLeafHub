0c140123e8a0395da33029dc1656d13035e12aaa
anruozhu <1485156774@qq.com>
2024-04-08 10:54:26 +0800

    添加阿里云的手机短信服务实现发送验证码

--- src/main/java/com/anranruozhu/service/impl/SmsServiceImpl.java
+++ src/main/java/com/anranruozhu/service/impl/SmsServiceImpl.java
@@ -33,8 +33,7 @@ public class SmsServiceImpl implements SmsService {
             if(StringUtils.isEmpty(phone)) return false;
 
             //default 地域节点，默认就好  后面是 阿里云的 id和秘钥（这里记得去阿里云复制自己的id和秘钥哦）
-           DefaultProfile profile = DefaultProfile.getProfile("default", "", "");
-            IAcsClient client = new DefaultAcsClient(profile);
+
 
             //这里不能修改
             CommonRequest request = new CommonRequest();
