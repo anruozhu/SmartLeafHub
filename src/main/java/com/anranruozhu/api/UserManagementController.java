b2e1d9ff20a8206c952e10e637a3c3ef6724bcb1
anruozhu <1485156774@qq.com>
2024-04-09 18:01:11 +0800

    对用户的密码以及用户的个人信息的服务进行划分为特定的接口

---
+++ src/main/java/com/anranruozhu/api/UserManagementController.java
@@ -0,0 +1,57 @@
+package com.anranruozhu.api;
+
+import com.anranruozhu.common.Result;
+import com.anranruozhu.entity.UserInfo;
+import com.anranruozhu.service.LoginService;
+import com.anranruozhu.service.UserInfoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author anranruozhu
+ * @ClassName UserManagementController
+ * @Description 用户信息管理
+ * @create 2024/4/9 下午5:49
+ **/
+@RestController
+@RequestMapping("/secure")
+@Slf4j
+public class UserManagementController {
+    @Autowired
+    private UserInfoService userInfoService;
+    @Autowired
+    private LoginService loginService;
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+    @PostMapping("/SetUserInfo")
+    public Result SetUserInfo(@RequestBody UserInfo userInfo, HttpServletRequest request){
+        int id= (int) request.getAttribute("id");
+        log.info("id==="+id);
+        userInfo.setId(id);
+        return userInfoService.UpdateUserInfo(userInfo);
+    }
+    @PostMapping("/UpdatePassword")
+    public Result UpdatePassword(@RequestParam String code1, @RequestParam String phone , @RequestParam String NewPassword, HttpServletRequest request){
+        // 处理注册逻辑
+        // 返回注册结果
+        //1、从redis中获取验证码，如果获取到就直接返回
+        String code = redisTemplate.opsForValue().get(phone);
+        int id= (int) request.getAttribute("id");
+
+        log.info(code);
+        Result rs=new Result();
+        rs.setCode(500);
+        rs.setMsg("验证码错误");
+        if(code1.equals(code) && loginService.isIdOk(id,phone)){
+            //验证码验证注册后删除对应值
+            redisTemplate.delete(phone);
+            return loginService.UpdatePassword(phone,NewPassword);
+        }else{
+            return rs;
+        }
+    }
+}
